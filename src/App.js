4f5dcbd24798db836e0c3edfa5b8757cfead1fec
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-04-23 20:19:26 -0300

    Alterações
    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 
 import './App.css';
+import './components/HelloWorld';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
 
-  const name = "Yuri"
-
-  const newName = name.toUpperCase()
-
-  function sum( a, b){
-    return (a+b)
-  }
 
 
   return (
     <div className="App">
-     <h1>Olá React</h1>
-     <h2>{name}</h2>
-     <p>Soma: {sum(1,2)}</p>
+     <HelloWorld/>
     </div>
   );
 }
