28c2353bf26cd176b708fa15b6a793b2e381eb90
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-04-23 20:33:51 -0300

    Utilizando Props
    Props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 
 import './App.css';
-import './components/HelloWorld';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
 
-
-
+  const nome = 'GEOBABY'
   return (
     <div className="App">
      <HelloWorld/>
+     <SayMyName nome="Matheus"/>
+     <SayMyName nome="Yuri"/>
+     <SayMyName nome={nome}/>
+     <Pessoa 
+     nome="Yuri" 
+     idade="33"
+     profissao='Programador'
+     foto="https://via.placeholder.com/150"
+     />
     </div>
   );
 }
