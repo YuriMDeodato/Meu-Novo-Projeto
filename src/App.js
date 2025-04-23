912f76a7edc043054a9c25fd18c605e8e97fd911
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-04-22 22:19:11 -0300

    Teste de funções
    Soma

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ function App() {
 
   const newName = name.toUpperCase()
 
+  function sum( a, b){
+    return (a+b)
+  }
+
 
   return (
     <div className="App">
      <h1>Olá React</h1>
      <h2>{name}</h2>
+     <p>Soma: {sum(1,2)}</p>
     </div>
   );
 }
