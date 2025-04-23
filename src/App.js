a60e1c6bdcbbabcc048664c5cb70e80b4f53c863
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-04-22 22:15:29 -0300

    Uso do toUpperCase
    Imprimir em maiúsculo

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 function App() {
 
+  const name = "Yuri"
+
+  const newName = name.toUpperCase()
 
 
   return (
     <div className="App">
      <h1>Olá React</h1>
+     <h2>{name}</h2>
     </div>
   );
 }
