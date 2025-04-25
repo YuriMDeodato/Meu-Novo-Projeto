065edb87f2f7830c1fcb6f39fe85b4d1834df2d9
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-04-25 18:35:27 -0300

    Importar e utilizar CSS
    Importar e utilizar CSS como classes no componente.

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from './Frase.module.css'
+
 function Frase(){
 
     return (
-        <div>
+        <div className={styles.p}>
             <p>Este é um componente com uma frase</p>
         </div>
     )
