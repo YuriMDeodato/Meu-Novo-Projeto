58727a2daa6f65a7975a2b56ce301c7b7f7d4769
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-04-25 22:59:10 -0300

    Props em lista
    .

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import Item from './Item'
+
+function List(){
+
+    return(
+        <>
+        <h1>Minha Lista</h1>
+            <ul>
+               <Item marca="Volto"/>
+               <Item marca="Volto"/>
+            </ul>
+        </>
+    )
+}
+
+export default List
