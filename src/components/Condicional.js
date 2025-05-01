b4581ea21cdd56a93a09f76b3cedb18529b873e2
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-04-30 22:22:59 -0300

    Excluir
    delete

--- src/components/Condicional.js
+++ src/components/Condicional.js
@@ -6,9 +6,13 @@ function Condicional(){
         setUserEmail(email); // Atualiza o estado com o e-mail digitado    
     }
 
+    function limparEmail(){
+        setUserEmail(""); // Limpa o estado do e-mail do usuário
+    }
+
     const [email, setEmail] = useState();
     const [userEmail, setUserEmail] = useState(); // Estado para armazenar o e-mail do usuário
-    
+
     return(
         <div>
             <h2>Cadastre o seu E-mail:</h2>
@@ -16,7 +20,15 @@ function Condicional(){
                 <input type="email" placeholder="Digite seu e-mail..." onChange={(e) =>setEmail(e.target.value)}/>
                 <button type="submit" onClick={enviarEmail}>Enviar E-mail</button>
             </form>
-
+            {userEmail && ( // Renderiza o parágrafo apenas se userEmail não for nulo ou indefinido
+                <p>O e-mail cadastrado foi: {userEmail}</p>
+            )}
+            <button onClick={limparEmail}></button> {/* Botão para limpar o e-mail cadastrado */} 
+            {userEmail ? ( // Se userEmail não for nulo ou indefinido, renderiza a mensagem abaixo
+                <p>Obrigado por cadastrar o seu e-mail!</p>
+            ) : ( // Caso contrário, renderiza a mensagem abaixo
+                <p>Por favor, cadastre o seu e-mail.</p>
+            )}  
         </div>
     )
 }
