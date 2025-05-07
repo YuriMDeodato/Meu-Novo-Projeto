c984de805c14763d0c83f5d9405528b99f4c3402
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-05-06 22:10:45 -0300

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
-import Condicional from './components/Condicional';
-import OutraLista from './components/OutraLista';
-import { useState } from 'react';
+
 import './App.css';
 
-const itens = [ "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" ];
 function App() {
 
 
   return (
     <div className="App">
-      <OutraLista item = {itens}/>
-      <OutraLista item = {[]}/>
+      
     </div>
   );
 }
