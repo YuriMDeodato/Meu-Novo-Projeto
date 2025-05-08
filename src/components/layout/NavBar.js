439f5cd0fb1f1ecceea93f84d0af2d41d168814f
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-05-08 12:12:30 -0300

    Alterações
    Alterações

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,12 +4,12 @@ import styles from './NavBar.module.css';
 function NavBar(){
 
     return(
-    <nav className={styles.navbar}>
-        <ul class name={styles.list}> 
-            <Link to="/">Home</Link>
-            <Link to="/empresa">Empresa</Link>
-            <Link to="/contato">Contato</Link>      
-            <Link to="/novoprojeto">Novo Projeto</Link>
+        <nav className={styles.navbar}>
+        <ul class={styles.list}>
+            <li className={styles.item}><Link to='/'>Home</Link></li>
+            <li className={styles.item}><Link to='/novoprojeto'>Projetos</Link></li>
+            <li className={styles.item}><Link to='/contato'>Contato</Link></li>
+            <li className={styles.item}><Link to='/empresa'>Empresa</Link></li>
         </ul>
     </nav>
     )
