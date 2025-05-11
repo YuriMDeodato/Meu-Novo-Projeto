4dac4f070cef67a9b866e441686efcdd7a7b88b8
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-05-11 20:46:52 -0300

    .
    .

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
 
 
   return (
-    
       <Router>
         <NavBar/>
         <Container customClass="min-height">
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import styles from './NavBar.module.css';
 import Container from "../layout/Container";
 import gif from "../../images/gifgif.gif";
+
 function NavBar(){
 
     return(
         
         <nav className={styles.navbar}>
+            <Link to='/'><img src={gif} alt="logo" className={styles.logo} /></Link>
             <Container className={styles.container}>
-            <Link class={styles.gif} to='/'><img src={gif} alt="logo"/></Link>
             <ul className={styles.list}>
             <li class={styles.item}><Link to='/'>Home</Link></li>
             <li class={styles.item}><Link to='/contato'>Contato</Link></li>
