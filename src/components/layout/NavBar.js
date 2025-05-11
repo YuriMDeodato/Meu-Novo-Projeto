fa4ed382cf6809c1d4f7cce1df68ee6fd7855ea9
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-05-11 20:24:52 -0300

    .
    .

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import styles from './NavBar.module.css';
 import Container from "../layout/Container";
+import gif from "../../images/gifgif.gif";
 function NavBar(){
 
     return(
         
         <nav className={styles.navbar}>
             <Container className={styles.container}>
-            <Link to='/'></Link>
+            <Link class={styles.gif} to='/'><img src={gif} alt="logo"/></Link>
             <ul className={styles.list}>
             <li class={styles.item}><Link to='/'>Home</Link></li>
             <li class={styles.item}><Link to='/contato'>Contato</Link></li>
