33fe165286fffd855fa39fa1d850de40bb89065a
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-05-13 19:05:24 -0300

    .
    .

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,13 @@
 
 import styles from './Footer.module.css';
 import {FaFacebook, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+
+
 function Footer(){
 
     return(
         <footer className={styles.footer}>
-            <ul>
+            <ul className={styles.social_list}>
             <p>SeuProjetinho &copy; 2025</p>
             <li><FaFacebook/></li>
             <li><FaInstagram/></li>
