9600a040aab9d2cc5abf6e9cc060e26a7ab62386
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-05-15 17:52:56 -0300

    .
    .

--- src/components/pages/NovoProjeto.js
+++ src/components/pages/NovoProjeto.js
@@ -1,11 +1,13 @@
-
+import styles from './NovoProjeto.module.css';
 
 function NovoProjeto(){
 
     return(
-        
-            <h1>NovoProjeto</h1>
-        
+        <div className={styles.novoprojeto_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p>formulário</p>
+        </div>
     )
 }
 
