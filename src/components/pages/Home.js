8615c1bd61747f343eab2dbbf2419feff838740a
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-05-14 23:34:33 -0300

    botões
    ...

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,16 @@
 import styles from './Home.module.css';
 import savings from '../../images/savings.svg';
+import LinkBotao from '../layout/LinkBotao';
+import { Link } from 'react-router-dom';
+
 function Home(){
 
     return(
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Gerenciador de Projetos</span></h1>
-            <p>Comece a gerar e gerenciar seu projetos agora mesmo!</p>
-            <button><a href="/">Criar Projeto</a></button>
+            <p className={styles.paragrafo}>Comece a gerar e gerenciar seu projetos agora mesmo!</p>
+            <button><a href="/novoprojeto">Criar Projeto</a></button>
+            <LinkBotao to="/novoprojeto" text="criar projeto" />
             <img src={savings} alt="Gerenciador de Projetos" />
         </section>
     )
