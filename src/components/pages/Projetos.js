154d01209133405e1ad77d1d4cf5740a1d1c112d
YuriMDeodato <146349459+YuriMDeodato@users.noreply.github.com>
2025-05-24 22:01:49 -0300

    .
    .

--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -1,5 +1,8 @@
 import Mensagens from "../layout/Mensagens";
 import { useLocation } from "react-router-dom";
+import styles from "./Projetos.module.css";
+import Container from "../layout/Container";
+import LinkBotao from "../layout/LinkBotao";
 
 function Projetos() {
 
@@ -10,10 +13,15 @@ function Projetos() {
   }
 
   return (
-    <div>
-      <h1>Projetos</h1>
-      <p>Esta é a página de projetos.</p>
+    <div className={styles.projeto_container}>
+      <div className={styles.titulo_container}>
+        <h1>Meus Projetos</h1>
+        <LinkBotao to="/novoprojeto" text="Criar Projeto" />
+      </div>
       {msg && <Mensagens type="sucesso" msg={msg} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     
     </div>
   );
